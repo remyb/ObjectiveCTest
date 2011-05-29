679ac74a52c7ceb2397306d02e0e0ac558bdea06
Remy Baumgarten <remy.baumgarten@gmail.com>
2011-05-29 02:36:54 -0400

    Added the rest

--- Testing/Testing/RadioStation.m
+++ Testing/Testing/RadioStation.m
@@ -10,5 +10,43 @@
 
 
 @implementation RadioStation
++(double) minAMFrequency {
+    return 520.0;
+}
++ (double) maxAMFrequency {
+    return 1610;
+}
++ (double) minFMFrequency {
+    return 88.3;
+}
++ (double) maxFMFrequency {
+    return 107.9;
+}
+
+- (id)initWithName:(NSString *)newName atFrequency:(double)freq {
+    self = [super init];
+    if (self != nil) {
+        name = newName;
+        frequency = freq;
+    }
+    return self;
+}
+- (NSString *)name {
+    return name;
+}
+- (void)setName:(NSString *)newName {
+    name = newName;
+}
+- (double)frequency {
+    return frequency;
+}
+- (void)setFrequency:(double)newFrequency {
+    frequency = newFrequency;
+}
+
+- (void) dealloc {
+    [name release];
+    [super dealloc];
+}
 
 @end
--- Testing/Testing/main.m
+++ Testing/Testing/main.m
@@ -7,15 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "RadioStation.h"
 int main (int argc, const char * argv[])
 {
 
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
-
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    NSMutableDictionary* stations = [[NSMutableDictionary alloc]init];
+    RadioStation* newStation;
+    newStation = [[RadioStation alloc]initWithName:@"Star 94 FM" atFrequency:94.1];
+    [stations setObject:newStation forKey:@"WSTR"];
+    [newStation release];
+    
+    NSLog(@"aaa%@", [stations objectForKey:@"WSTR"]);
+    [stations release];
+    
     [pool drain];
     return 0;
 }
